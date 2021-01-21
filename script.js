eb4b74fa76dfb979e00a8a4ed6516839014f9c16
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-21 10:41:48 -0800

    Fix typo

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// Create ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+};
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
